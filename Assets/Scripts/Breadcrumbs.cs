c1de51ac337f4dc1006a4008a24b294e9e690aa3
danny6430 <danny6430@hotmail.com>
2018-02-07 17:49:10 -0700

    first commit

---
+++ Assets/Scripts/Breadcrumbs.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breadcrumbs : MonoBehaviour
+{
+    public GameObject prefab;
+    float time = 30.0f;
+
+    int count = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+    private void FixedUpdate()
+    {
+        Debug.Log(Time.fixedDeltaTime);
+
+        if (time >= 0.0f && count == 0)
+        {
+            Instantiate(prefab, transform.position, Quaternion.identity);
+        }
+
+        time -= Time.fixedDeltaTime;
+
+        count++;
+
+        if (count >= 10)
+            count = 0;
+    }
+}
---
+++ Assets/Scripts/SystemIO.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SystemIO : MonoBehaviour
+{
+    string path = "C:\\Users\\u1070737\\Desktop\\Test2.txt";
+
+    Kinematic Kinematic;
+    StreamWriter sw;
+
+    float time = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Kinematic = GetComponent<Kinematic>();
+
+        if (!File.Exists(path))
+        {
+            sw = File.CreateText(path);
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        Debug.Log(Time.fixedDeltaTime);
+
+        if (time >= 0.0f && sw != null)
+        {
+            Vector2 v = new Vector2(Kinematic.getVelocity().x, Kinematic.getVelocity().z);
+            Debug.Log(v.magnitude);
+            sw.WriteLine(v.magnitude);
+        }
+        else
+        {
+            sw.Close();
+            Debug.Log("Done");
+        }
+
+
+        time -= Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+
+
+
+	}
+}
